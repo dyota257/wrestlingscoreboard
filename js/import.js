8d06b54f3b21ccc6587f3b90bd8a293fc6ecabc0
Dyota Tanuwibawa <dyota257@gmail.com>
2020-08-12 23:47:43 +0800

    Working roster table import from Excel format

--- js/import.js
+++ js/import.js
@@ -4,39 +4,69 @@ var pairing = [];
 var table = [];
 var blueFirstName = "";
 var blueLastName = "";
+var blueClubName = "";
 var redFirstName = "";
 var redLastName = "";
+var redClubName = "";
+var raw = "";
+var rows = [];
+var tablePage = $("table[name=list]");
 
 $("button#import").click( function() {
-    var raw  =  $("textarea").val();
     
-    var rows = raw.split("\n");
+    tablePage.html("");
+
+    raw  =  $("textarea").val();
+
+    rows = raw.trim().split("\n");
 
     for (var i=0; i<rows.length; i++) {
         var names  = [
-            rows[i].split("\t")[4], //red side
-            rows[i].split("\t")[5] //blue side
+            rows[i].split("\t")[0], //red side
+            rows[i].split("\t")[1] //blue side
         ]
         
+        table[i] = names;
+        console.log(names);
+        console.log(names[1]);
         table.push(names);
 
+        redFirstName = names[0].split(" ")[0];
+        redLastName = names[0].split(" ")[1];
+        redClubName = names[0].split(" ")[2];
+        blueFirstName = names[1].split(" ")[0];
+        blueLastName = names[1].split(" ")[1];
+        blueClubName = names[1].split(" ")[2];
+
+        tablePage.html(
+            tablePage.html() + 
+            "<tr matchOrder="+i+"><td>"+redFirstName+" "+redLastName+" "+redClubName+"</td><td>"+blueFirstName+" "+blueLastName+" "+blueClubName+"</td></tr>"
+        )
+        
     }
 
     matchOrder = 0;
     updateNames(matchOrder);
+    $("textarea").val("");
+    $("tr[matchOrder='0']").addClass("matchOrder");
+    
 
 })
 
 
 
 $("button#next").click( function() {
-    matchOrder++;
+    $("tr").removeClass("matchOrder")
+    if (matchOrder!= table.length) {matchOrder++};
     updateNames(matchOrder);
+    $("tr[matchorder='"+matchOrder+"']").addClass("matchOrder");
 })
 
 $("button#prev").click( function() {
+    $("tr").removeClass("matchOrder")
     if (matchOrder!=0) {matchOrder--;}
     updateNames(matchOrder);
+    $("tr[matchorder='"+matchOrder+"']").addClass("matchOrder");
 })
 
 function updateNames(matchOrder) {
