6320890c66cbe3ad0746c36e97c0d9375b6f32e7
Dyota Tanuwibawa <dyota257@gmail>
2020-11-30 22:05:03 +0800

    Add class 'mobile' to fixtures table elements. Add file mobile.css to handle responsive sizing. Add mobile.css to boilerplate.ejs

--- database/matchesToHtml.js
+++ database/matchesToHtml.js
@@ -10,35 +10,35 @@ function matchesToHtml(x) {
         
         tableRows = tableRows+
         `
-        <tr matchOrder="${i}">
-            <td>
+        <tr class="mobile" matchOrder="${i}">
+            <td class="mobile" data-label="category">
                 ${x[i].category}
             </td>
-            <td>
+            <td class="mobile" data-label="round">
                 ${x[i].round}
             </td>
-            <td style='${colorRed};'>
+            <td class="mobile" data-label="red_name" style='${colorRed};'>
                 ${x[i].red_name}
             </td>
-            <td style='${colorRed};display:none'>
+            <td class="mobile" data-label="red_firstname" style='${colorRed};display:none'>
                 ${x[i].red_firstname}
             </td>
-            <td style='${colorRed};display:none'>
+            <td class="mobile" data-label="red_lastname" style='${colorRed};display:none'>
                 ${x[i].red_lastname}
             </td>
-            <td style='${colorRed};'>
+            <td class="mobile" data-label="red_club" style='${colorRed};'>
                 ${x[i].red_club}
             </td>
-            <td style='${colorBlue};'>
+            <td class="mobile" data-label="blue_name" style='${colorBlue};'>
                 ${x[i].blue_name}
             </td>
-            <td style='${colorBlue};display:none'>
+            <td class="mobile" data-label="blue_firstname" style='${colorBlue};display:none'>
                 ${x[i].blue_firstname}
             </td>
-            <td style='${colorBlue};display:none'>
+            <td class="mobile" data-label="blue_lastname" style='${colorBlue};display:none'>
                 ${x[i].blue_lastname}
             </td>
-            <td style='${colorBlue};'>
+            <td class="mobile" data-label="blue_club" style='${colorBlue};'>
                 ${x[i].blue_club}
             </td>
         </tr>
@@ -46,14 +46,14 @@ function matchesToHtml(x) {
         `
     }
 
-    tableRows = `<table name='list'>
-        <tr style='font-weight:bold; text-align:center;'>
-            <td>Category</td>
-            <td>Round</td>
-            <td>Red</td>
-            <td>Red Club</td>
-            <td>Blue</td>
-            <td>Blue Club</td>
+    tableRows = `<table class="mobile" name='list'>
+        <tr class="mobile"style='font-weight:bold; text-align:center;'>
+            <td class="mobile">Category</td>
+            <td class="mobile">Round</td>
+            <td class="mobile">Red</td>
+            <td class="mobile">Red Club</td>
+            <td class="mobile">Blue</td>
+            <td class="mobile">Blue Club</td>
         </tr>
     ${tableRows}</table>`
 
